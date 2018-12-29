73cda1334b11e803b7f7a9c03bd44263b3a7e874
spounka <accelerator346@outlook.com>
2018-12-29 12:45:36 +0100

    Minor changes

--- static/js/topBar.js
+++ static/js/topBar.js
@@ -8,3 +8,8 @@ function closeMenu()
     var obj = document.getElementById("sideMenu");
     obj.style.width = "0px";
 }
+
+function stick()
+{
+    var yOffset = document.getElementById("TopBar").
+}
